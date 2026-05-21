4e37b952defa6b7c3c8363290bdae6e1734779ca
developuseful <92522986+developuseful@users.noreply.github.com>
2026-05-22 01:34:37 +0800

    feat: Update UseCaseModule with local and import use cases

--- app/src/main/java/com/develop/films/di/UseCaseModule.kt
+++ app/src/main/java/com/develop/films/di/UseCaseModule.kt
@@ -2,11 +2,16 @@ package com.develop.films.di
 
 import com.develop.films.domain.repository.MovieRepository
 import com.develop.films.domain.use_case.AddMovieUseCase
+import com.develop.films.domain.use_case.AddLocalMovieUseCase
 import com.develop.films.domain.use_case.DeleteMovieUseCase
 import com.develop.films.domain.use_case.GetMovieByIdFlowUseCase
 import com.develop.films.domain.use_case.GetMovieByIdUseCase
 import com.develop.films.domain.use_case.GetMoviesUseCase
 import com.develop.films.domain.use_case.UpdateMovieUseCase
+import com.develop.films.domain.use_case.ImportLocalMovieToAccountUseCase
+import com.develop.films.data.repository.MovieImportManager
+import com.google.firebase.database.FirebaseDatabase
+import android.app.Application
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,6 +25,10 @@ object UseCaseModule {
     @Provides
     @Singleton
     fun provideAddMovieUseCase(repository: MovieRepository) = AddMovieUseCase(repository)
+    
+    @Provides
+    @Singleton
+    fun provideAddLocalMovieUseCase(repository: MovieRepository) = AddLocalMovieUseCase(repository)
 
     @Provides
     @Singleton
@@ -40,4 +49,17 @@ object UseCaseModule {
     @Provides
     @Singleton
     fun provideUpdateMovieUseCase(repository: MovieRepository) = UpdateMovieUseCase(repository)
+    
+    @Provides
+    @Singleton
+    fun provideMovieImportManager(
+        firebaseDatabase: FirebaseDatabase,
+        app: Application
+    ) = MovieImportManager(firebaseDatabase, app)
+    
+    @Provides
+    @Singleton
+    fun provideImportLocalMovieToAccountUseCase(
+        importManager: MovieImportManager
+    ) = ImportLocalMovieToAccountUseCase(importManager)
 }
