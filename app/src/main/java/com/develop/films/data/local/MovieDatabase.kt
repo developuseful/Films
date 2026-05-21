c14640beb31be276047cf8d4561a29b7610c2ddd
developuseful <92522986+developuseful@users.noreply.github.com>
2026-05-22 01:36:55 +0800

    feat: Update MovieDatabase version to 3

--- app/src/main/java/com/develop/films/data/local/MovieDatabase.kt
+++ app/src/main/java/com/develop/films/data/local/MovieDatabase.kt
@@ -6,7 +6,7 @@ import com.develop.films.data.local.entity.MovieEntity
 
 @Database(
     entities = [MovieEntity::class],
-    version = 2,
+    version = 3,  // ✅ Обновлено с 2 на 3
     exportSchema = false
 )
 abstract class MovieDatabase : RoomDatabase() {
